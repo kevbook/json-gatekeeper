a7fd9ca4c476324534376c69491d62f68f52cfaf
Karan Sakhuja <kevin.sakhuja@gmail.com>
2015-03-10 19:44:41 -0400

    Fixes

--- tests/build.js
+++ tests/build.js
@@ -5,7 +5,6 @@ module.exports = function(Schema, jsonGatekeeper) {
 
   describe('Build schema', function() {
 
-    // ******** Test ********/
     it('should error out, str4 key has a few errors', function() {
       var s = Schema();
 
@@ -16,9 +15,7 @@ module.exports = function(Schema, jsonGatekeeper) {
       .throwError(/str4/);
     });
 
-
-    // ******** Test ********/
-    it('should error out, str5 key has a doesnt have proper format key', function() {
+    it('should error out, str5 key doesnt have proper format key - format array is empty', function() {
       var s = Schema();
       delete s.str4;
 
@@ -29,12 +26,11 @@ module.exports = function(Schema, jsonGatekeeper) {
       .throwError(/array/i);
     });
 
-
-    // ******** Test ********/
-    it('should error out, str6 key has a doesnt have proper format value', function() {
+    it('should error out, str6 key doesnt have proper format value', function() {
       var s = Schema();
       delete s.str4;
       delete s.str5;
+      delete s.missing_type;
 
       (function() {
         jsonGatekeeper(s)
@@ -43,6 +39,207 @@ module.exports = function(Schema, jsonGatekeeper) {
       .throwError(/format/i);
     });
 
+    it('should error out, "wrong_type" key has wrong type', function() {
+      var s = Schema();
+      delete s.str4;
+      delete s.str5;
+      delete s.str6;
+      delete s.missing_type;
+
+      (function() {
+        jsonGatekeeper(s)
+      })
+      .should
+      .throwError(/type/i);
+    });
+
+    it('should error out, "missing_type" key has missing type', function() {
+      var s = Schema();
+      delete s.str4;
+      delete s.str5;
+      delete s.str6;
+      delete s.wrong_type;
+
+      (function() {
+        jsonGatekeeper(s)
+      })
+      .should
+      .throwError(/type/i);
+    });
+
+    it('should error out, number type has wrong default value', function() {
+      var s = Schema();
+      delete s.str4;
+      delete s.str5;
+      delete s.str6;
+      delete s.wrong_type;
+      delete s.missing_type;
+      delete s.number2;
+
+      (function() {
+        jsonGatekeeper(s)
+      })
+      .should
+      .throwError(/default/i);
+    });
+
+    it('should error out, number has wrong format', function() {
+      var s = Schema();
+      delete s.str4;
+      delete s.str5;
+      delete s.str6;
+      delete s.wrong_type;
+      delete s.missing_type;
+      delete s.number1;
+
+      (function() {
+        jsonGatekeeper(s)
+      })
+      .should
+      .throwError(/format/i);
+    });
+
+    it('should error out, date type has wrong default value', function() {
+      var s = Schema();
+      delete s.str4;
+      delete s.str5;
+      delete s.str6;
+      delete s.wrong_type;
+      delete s.missing_type;
+      delete s.number1;
+      delete s.number2;
+
+      (function() {
+        jsonGatekeeper(s)
+      })
+      .should
+      .throwError(/default/i);
+    });
+
+    it('should error out, date type has wrong default value', function() {
+      var s = Schema();
+      delete s.str4;
+      delete s.str5;
+      delete s.str6;
+      delete s.wrong_type;
+      delete s.missing_type;
+      delete s.number1;
+      delete s.number2;
+      delete s.date2;
+
+      (function() {
+        jsonGatekeeper(s)
+      })
+      .should
+      .throwError(/default/i);
+    });
+
+    it('should error out, date type has wrong default value', function() {
+      var s = Schema();
+      delete s.str4;
+      delete s.str5;
+      delete s.str6;
+      delete s.wrong_type;
+      delete s.missing_type;
+      delete s.number1;
+      delete s.number2;
+      delete s.date2;
+      delete s.date3;
+
+      (function() {
+        jsonGatekeeper(s)
+      })
+      .should
+      .throwError(/default/i);
+    });
+
+    it('should error out, boolean type has wrong default value', function() {
+      var s = Schema();
+      delete s.str4;
+      delete s.str5;
+      delete s.str6;
+      delete s.wrong_type;
+      delete s.missing_type;
+      delete s.number1;
+      delete s.number2;
+      delete s.date2;
+      delete s.date3;
+      delete s.date4;
+
+      (function() {
+        jsonGatekeeper(s)
+      })
+      .should
+      .throwError(/default/i);
+    });
+
+    it('should error out, date type has wrong validate value', function() {
+      var s = Schema();
+      delete s.str4;
+      delete s.str5;
+      delete s.str6;
+      delete s.wrong_type;
+      delete s.missing_type;
+      delete s.number1;
+      delete s.number2;
+      delete s.date2;
+      delete s.date3;
+      delete s.date4;
+      delete s.bool2;
+
+      (function() {
+        jsonGatekeeper(s)
+      })
+      .should
+      .throwError(/array/i);
+    });
+
+    it('should error out, date type has wrong validate value', function() {
+      var s = Schema();
+      delete s.str4;
+      delete s.str5;
+      delete s.str6;
+      delete s.wrong_type;
+      delete s.missing_type;
+      delete s.number1;
+      delete s.number2;
+      delete s.date2;
+      delete s.date3;
+      delete s.date4;
+      delete s.bool2;
+      delete s.date8;
+
+      (function() {
+        jsonGatekeeper(s)
+      })
+      .should
+      .throwError(/validate/i);
+    });
+
+    it('should error out, null type has wrong default', function() {
+      var s = Schema();
+      delete s.str4;
+      delete s.str5;
+      delete s.str6;
+      delete s.wrong_type;
+      delete s.missing_type;
+      delete s.number1;
+      delete s.number2;
+      delete s.date2;
+      delete s.date3;
+      delete s.date4;
+      delete s.bool2;
+      delete s.date8;
+      delete s.date9;
+
+      (function() {
+        jsonGatekeeper(s)
+      })
+      .should
+      .throwError(/default/i);
+    });
+
+
 
     // ******** Test ********/
     it('should work, returns an object with "schema" key', function() {
@@ -50,6 +247,17 @@ module.exports = function(Schema, jsonGatekeeper) {
       delete s.str4;
       delete s.str5;
       delete s.str6;
+      delete s.wrong_type;
+      delete s.missing_type;
+      delete s.number1;
+      delete s.number2;
+      delete s.date2;
+      delete s.date3;
+      delete s.date4;
+      delete s.bool2;
+      delete s.date8;
+      delete s.date9;
+      delete s.null1;
 
       jsonGatekeeper(s)
         .should
@@ -57,8 +265,6 @@ module.exports = function(Schema, jsonGatekeeper) {
         .have.keys('schema');
     });
 
-
   });
-
 
 };
--- tests/index.js
+++ tests/index.js
@@ -36,6 +36,136 @@ var schema = {
     type: 'string',
     format: [ function(v){return v+'superman'} ]
   },
+  wrong_type: {
+    type: 'crap'
+  },
+  missing_type: {
+    required: true
+  },
+  number1: {
+    type: 'number',
+    required: true,
+    default: 'superman',
+    format: ['trim','camelCase']
+  },
+  number2: {
+    type: 'number',
+    required: true,
+    default: -123.4444,
+    format: ['trim','camelCase']
+  },
+  number3: {
+    type: 'number',
+    required: true,
+    default: -123.4444,
+    format: ['toInt']
+  },
+  number4: {
+    type: 'number'
+  },
+  date1: {
+    type: 'date',
+    required: true
+  },
+  date2: {
+    type: 'date',
+    required: true,
+    default: 'sss'
+  },
+  date3: {
+    type: 'date',
+    required: true,
+    default: '10-1-2015'
+  },
+  date4: {
+    type: 'date',
+    required: true,
+    default: Date.parse('10-1-2015')
+  },
+  date5: {
+    type: 'date',
+    required: true,
+    default: new Date('10-1-2015')
+  },
+  date6: {
+    type: 'date',
+    required: true,
+    default: new Date('10-1-2015'),
+    typeCast: true
+  },
+  date7: {
+    type: 'date',
+    required: true,
+    default: new Date('10-1-2015'),
+    typeCast: true,
+    format: []
+  },
+  bool1: {
+    type: 'boolean',
+    required: true
+  },
+  bool2: {
+    type: 'boolean',
+    required: true,
+    default: 'super'
+  },
+  bool3: {
+    type: 'boolean',
+    required: true,
+    default: false
+  },
+  bool4: {
+    type: 'boolean',
+    required: true,
+    default: false,
+    typeCast: true,
+    format: 'sss'
+  },
+  bool5: {
+    type: 'boolean',
+    required: true,
+    default: false,
+    typeCast: true,
+    validate: 'sss'
+  },
+  date8: {
+    type: 'date',
+    required: true,
+    default: new Date('10-1-2015'),
+    typeCast: true,
+    format: [],
+    validate: 'sss'
+  },
+  date9: {
+    type: 'date',
+    required: true,
+    default: new Date('10-1-2015'),
+    typeCast: true,
+    format: [],
+    validate: ['sss']
+  },
+  date10: {
+    type: 'date',
+    required: true,
+    default: new Date('10-1-2015'),
+    typeCast: true,
+    format: [],
+    validate: ['isAfter()']
+  },
+  null1: {
+    type: 'null',
+    required: true,
+    default: false,
+    validate: 'sss'
+  },
+  null2: {
+    type: 'null',
+    required: true,
+    default: null
+  },
+
+
+
 
 
 /******
